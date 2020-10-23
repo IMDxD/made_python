4afeb115990ddd06cbde273ef0cf2d592a1a7b99
IMDxD <nestor969@mail.ru>
2020-10-23 14:47:37 +0300

    inverted_index_starter.py interface test_inverted_index_starter.py started test

---
+++ inverted_index/test_inverted_index_starter.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_class_import():
+    from inverted_index_starter import InvertedIndex
+
+
+def test_class_construct():
+    from inverted_index_starter import InvertedIndex
+    ii = InvertedIndex()
+
